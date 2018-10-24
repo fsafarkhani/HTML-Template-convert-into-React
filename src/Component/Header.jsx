0c01688ca48cd76a95cc4def23ca71de8dac8499
fsafarkhani <fereshteh.safarkhani@yahoo.com>
2018-10-24 15:03:48 +0330

    pinnecs done logo

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -34,7 +34,7 @@ export default class Header extends Component {
           {/* Navbar */}
           <nav className="navbar navbar-expand-lg navbar-light" id="mainNav" data-toggle="affix">
             <div className="container">
-              <a className="navbar-brand smooth-scroll" href="index.html">
+              <a className="navbar-brand smooth-scroll" href="/">
                 <img src="img/logo-s.png" alt="logo" />
               </a>
               <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
